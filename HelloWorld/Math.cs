47dd6191aafc89ec989616b3ad8d0c26799bd274
HARDPC <HARDPC@WOTUM>
2023-12-01 19:34:21 +0100

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Math
+    {
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+         
+    }
+}
